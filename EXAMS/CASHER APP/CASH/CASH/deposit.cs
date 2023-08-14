4ba7ecc1acf8a9f7a6c665b77da69e146b5ffa17
Mahmut DEMİRKIRAN <mahmutdmrkrn14@hotmail.com>
2023-08-14 18:02:20 +0300

    CASHER APP
    CASHER APP

---
+++ EXAMS/CASHER APP/CASH/CASH/deposit.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CASH
+{
+    public partial class deposit : Form
+    {
+        public deposit()
+        {
+            InitializeComponent();
+        }
+
+        public string conString = "Data Source=PC-ISA\\SQLEXPRESS;Initial Catalog=Casher;Integrated Security=True";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (int.TryParse(textBox1.Text, out int paraMiktari) && int.TryParse(textBox2.Text, out int musteriID))
+                {
+                    SqlConnection conn = new SqlConnection(conString);
+                    conn.Open();
+
+                    string selectQuery = $"SELECT Bakiye FROM Musteri WHERE Musteri_ID = {musteriID}";
+                    SqlCommand selectCmd = new SqlCommand(selectQuery, conn);
+
+                    int mevcutBakiye = Convert.ToInt32(selectCmd.ExecuteScalar());
+                    int yeniBakiye = mevcutBakiye + paraMiktari;
+
+                    string updateQuery = $"UPDATE Musteri SET Bakiye = {yeniBakiye} WHERE Musteri_ID = {musteriID}";
+                    SqlCommand updateCmd = new SqlCommand(updateQuery, conn);
+                    int affectedRows = updateCmd.ExecuteNonQuery();
+
+                    if (affectedRows > 0)
+                    {
+                        label1.Text = "Yeni Bakiye: " + yeniBakiye.ToString();
+                        MessageBox.Show("Bakiye güncellendi.");
+
+                        // TextBox değerlerini temizle
+                        textBox1.Text = "";
+                        textBox2.Text = "";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Bakiye güncellenemedi.");
+                    }
+
+                    conn.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Geçerli değerler girin.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+
+        }
+
+    }
+    }
+
